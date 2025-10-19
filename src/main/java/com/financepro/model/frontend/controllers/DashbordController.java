41e1e47506113d458c8fa4493bfd03db8e78286b
vitao-al <vm74531@gmail.com>
2025-10-19 18:58:31 -0300

    add features views 4

--- src/main/java/com/financepro/model/frontend/controllers/DashbordController.java
+++ src/main/java/com/financepro/model/frontend/controllers/DashbordController.java
@@ -1,19 +1,20 @@
 package com.financepro.model.frontend.controllers;
 
 import com.financepro.model.backend.dataTransferObjects.Usuario;
+import com.financepro.model.frontend.launcher.launcherPrincipal;
 import javafx.animation.TranslateTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import javafx.event.ActionEvent; // Importe ActionEvent
 
 public class DashbordController {
+
     @FXML
     private Text labelUsername;
     @FXML
@@ -22,52 +23,67 @@ public class DashbordController {
     private VBox slideMenu;
     @FXML
     private Button btnMenu;
-
+    @FXML
+    private Button showMetas;
     @FXML
     private PieChart teste;
 
+    private static final double ESPACO_DE_TRANSICAO = 10;
 
     private boolean isMenuOpen = false;
+
 
     public void initialize() {
 
         // Criando dados para o PieChart
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
-                        new PieChart.Data("Entradas", 40),
-                        new PieChart.Data("Despesas", 60)
+                        new PieChart.Data("Categoria1", 40),
+                        new PieChart.Data("Categoria2", 60),
+                        new PieChart.Data("Categoria3", 80)
                 );
 
         teste.setData(pieChartData);
-        teste.setTitle("Saldo Mensal");
-
 
         Usuario user = dadosGlobais.getUser();
         this.labelUsername.setText(dadosGlobais.user.getUsername());
         this.labelSaldo.setText(String.valueOf(user.getRenda()));
 
-        btnMenu.setOnAction(e -> {
-            TranslateTransition transition = new TranslateTransition(Duration.millis(300), slideMenu);
-
-            if (isMenuOpen == false) {
-                // Abrir
-                transition.setToX(250); // desloca o menu 250px para a direita
-                transition.play();
-                isMenuOpen = true;
-
-            } else {
-                // Fechar
-                transition.setToX(0);
-                transition.setOnFinished(event -> slideMenu.setLayoutX(-250)); // volta posição
-                slideMenu.setLayoutX(0);
-                transition.play();
-                isMenuOpen = false;
+        showMetas.setOnAction(event -> {
+            try {
+                launcherPrincipal.changeView("/views/viewMetas.fxml");
+            }catch(Exception ex){
+                throw new RuntimeException(ex);
             }
         });
 
-
     }
 
+    @FXML
+    private void handleMenuClick(ActionEvent event) {
+
+        TranslateTransition transition = new TranslateTransition(Duration.millis(300), slideMenu);
+
+        if (!isMenuOpen) {
+
+            transition.setToX(ESPACO_DE_TRANSICAO);
+
+
+            slideMenu.setVisible(true);
+
+            transition.play();
+            isMenuOpen = true;
+
+        } else {
+
+            transition.setToX(0);
+
+            transition.setOnFinished(e -> {
+                slideMenu.setVisible(false);
+            });
+
+            transition.play();
+            isMenuOpen = false;
+        }
+    }
 }
-
-
