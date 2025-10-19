c3f7574efa14528bc04f8b972e43290c1433b9c4
vitao-al <vm74531@gmail.com>
2025-10-18 22:07:13 -0300

    add features

--- src/main/java/com/financepro/model/frontend/controllers/DashbordController.java
+++ src/main/java/com/financepro/model/frontend/controllers/DashbordController.java
@@ -1,11 +1,20 @@
 package com.financepro.model.frontend.controllers;
 
+import com.financepro.model.backend.dataTransferObjects.Usuario;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
 
 public class DashbordController {
+    @FXML
+    private static Label mensagemBoasVindas;
 
-
-    public AnchorPane panemenu;
+    @FXML
+    public void initialize() {
+        mensagemBoasVindas.setText(dadosGlobais.user.getUsername());
+    }
 }
 
 
---
+++ src/main/java/com/financepro/model/frontend/controllers/dadosGlobais.java
@@ -0,0 +1,9 @@
+package com.financepro.model.frontend.controllers;
+
+import com.financepro.model.backend.dataTransferObjects.Usuario;
+
+public class dadosGlobais {
+
+    public static Usuario user;
+
+}
